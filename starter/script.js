2c21424e00ad588b02bf13603fb837889b10db5e
yashweybee <yashparmar@weybee.in>
2023-08-07 16:14:11 +0530

    completed

---
+++ starter/script.js
@@ -0,0 +1,120 @@
+// Coding Challenge-1
+
+// let weightMark = 78
+// let heightMark = 1.69
+
+// let weightJohn = 92
+// let heightJohn = 1.95
+
+// let BMIMark = weightMark / (heightMark * heightMark)
+// let BMIjohn = weightJohn / (heightJohn * heightJohn)
+
+// let markHigherBMI = BMIMark > BMIjohn;
+
+// console.log(markHigherBMI);
+
+
+
+// Coding Challenge-2
+
+// let weightMark = 78
+// let heightMark = 1.69
+
+// let weightJohn = 92
+// let heightJohn = 1.95
+
+// let BMIMark = weightMark / (heightMark * heightMark)
+// let BMIjohn = weightJohn / (heightJohn * heightJohn)
+
+// if(BMIMark > BMIjohn){
+//     console.log(`Mark's BMI is higher than John's`);
+// }else{
+//     console.log(`John's BMI is higher than Mark's`);
+// }
+
+
+// Coding Challenge-3
+
+// let dolphin1 = 54
+// let dolphin2 = 11
+// let dolphin3 = 10
+
+// let koalas1 = 10
+// let koalas2 = 12
+// let koalas3 = 12
+
+// let avgdophin = (dolphin1 + dolphin2 + dolphin3) / 3
+// let avgkoalas = (koalas1 + koalas2 + koalas3) / 3
+// console.log(avgdophin);
+// console.log(avgkoalas);
+
+// if (avgdophin > avgkoalas && avgdophin >= 100) {
+//     console.log("dolphin wins");
+// } else if (avgkoalas > avgdophin && avgkoalas >= 100) {
+//     console.log("koalas wins");
+// } else if (avgdophin === avgkoalas && avgdophin > 100 && avgkoalas > 100) {
+//     console.log("Both wins");
+// } else {
+//     console.log("no winners");
+// }
+
+// const day = "mon"
+
+// switch (day) {
+//     case "mon":
+//         console.log("do somthing");
+//         break;
+//     case "tue":
+//         console.log("tue do somthing");
+//         break;
+//     case "wed":
+//         console.log("wed do somthing");
+//         break;
+//     case "thu":
+//         console.log("thu do somthing");
+//         break;
+//     case "fri":
+//         console.log("do faster");
+//     case "sat":
+//     case "sun":
+//         console.log("do nothing");
+//         break;
+//     default:
+//         console.log("enter valid day");
+// }
+
+// if(day === "mon"){
+//     console.log("do somthing");
+// }
+// else if(day === "tue"){
+//     console.log("tue do somthing");
+// }
+// else if(day === "wed"){
+//     console.log("wed do somthing");
+// }
+// else if(day === "thu"){
+//     console.log("thu do somthing");
+// }
+// else if(day === "fri"){
+//     console.log("tue do somthing");
+// } else if(day === " sat" || day === "sun"){
+//     console.log("do nothing");
+// }
+
+
+// const age = 6;
+// const abcd =
+//     age >= 18
+//         ? "adult"
+//         : age <= 10
+//             ? "just born"
+//             : "child";
+// console.log(abcd);
+
+
+// challenge-4
+// let bill = 430;
+
+// let tip = (bill >= 50 && bill <= 300) ? (bill * 15) / 100 : (bill * 20) / 100;
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
